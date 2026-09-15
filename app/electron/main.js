541517b9f0f5e025758581adca8f612869033d88
Dovvahkiin <bozidartovarnickii@gmail.com>
2026-09-15 23:37:38 +0200

    Add new api for network scanning

--- app/electron/main.js
+++ app/electron/main.js
@@ -1,7 +1,13 @@
-import { app } from "electron";
+import { app, ipcMain } from "electron";
 import centralizedApp from "./services/appservice.js";
+import scanNetwork from "./services/nmap-service.js";
 
 const { readyApp, closeApp } = centralizedApp(app);
 
 readyApp();
+
+ipcMain.handle("scan-network", async () => {
+  return await scanNetwork();
+});
+
 closeApp();
