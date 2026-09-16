fd707dcf9017ba424e5dfb2b9e1b9a57ef7f45a3
Dovvahkiin <bozidartovarnickii@gmail.com>
2026-09-16 20:49:24 +0200

    Network scan now returns analized and compared data instead of raw nmap data

--- app/electron/main.js
+++ app/electron/main.js
@@ -1,13 +1,16 @@
 import { app, ipcMain } from "electron";
 import centralizedApp from "./services/appservice.js";
 import scanNetwork from "./services/nmap-service.js";
+import analyze from "./services/devices-analyzer.js";
 
 const { readyApp, closeApp } = centralizedApp(app);
 
 readyApp();
 
 ipcMain.handle("scan-network", async () => {
-  return await scanNetwork();
+  const data = await scanNetwork();
+  const analized = analyze(data);
+  return analized;
 });
 
 closeApp();
